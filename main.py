ebe83a6b09fdc3930a0310ad28a0858b7986b04c
YilunAllenChen <allenchenyilun1999@gmail.com>
2022-12-19 18:02:48 -0600

    formatting

--- main.py
+++ main.py
@@ -5,47 +5,47 @@ import threading
 
 
 from infra.rich_logging import getLogger, print, logging
-
 from portfolio import Portfolio
 from apis import ws_client, client, SYMBOL
 
+# strategy is parameterized with a config file to make strategy adjustments easier
 dir_path = os.path.dirname(os.path.realpath(__file__)) + "/config.json"
-with open(dir_path, "r") as f:
+with open(dir_path, 'r') as f:
     config = json.loads(f.read())
-    strat_1 = config["strategy1"]
+    strat_1 = config['strategy1']
 p = Portfolio(config=strat_1)
 
 
 def book_update_handler(message):
     try:
         new_quotes = {}
-        new_quotes["ask_px"] = float(message["a"][0][0])
-        new_quotes["ask_qty"] = float(message["a"][0][1])
-        new_quotes["bid_px"] = float(message["b"][0][0])
-        new_quotes["bid_qty"] = float(message["b"][0][1])
+        new_quotes['ask_px'] = float(message['a'][0][0])
+        new_quotes['ask_qty'] = float(message['a'][0][1])
+        new_quotes['bid_px'] = float(message['b'][0][0])
+        new_quotes['bid_qty'] = float(message['b'][0][1])
         # used for retreaing to next level
-        new_quotes["secondary_ask_px"] = float(message["a"][1][0])
-        new_quotes["secondary_bid_px"] = float(message["b"][1][0])
+        new_quotes['secondary_ask_px'] = float(message['a'][1][0])
+        new_quotes['secondary_bid_px'] = float(message['b'][1][0])
 
         p.update_quotes(new_quotes=new_quotes)
     except Exception as e:
         logging.error(f"Error parsing book update : {str(e)}", exc_info=True)
-
-
+    
 def account_update_handler(message):
     if message.get("e") == "ORDER_TRADE_UPDATE":
-        oid = message["o"]["i"]
-        new_status = message["o"]["X"]
+        oid = message['o']['i']
+        new_status = message['o']['X']
         if "FILL" in new_status:
             logging.info(f"order {oid} is now {new_status}")
     elif message.get("e") == "ACCOUNT_UPDATE":
-        positions = message["a"]["P"]
+        positions = message['a']['P']
         for position in positions:
-            if position["s"] == "BTCUSDT":
-                position_size = float(position["pa"])
+            if position['s'] == "BTCUSDT":
+                position_size = float(position['pa'])
                 p.update_position_size(position_size=position_size)
 
 
+listen_key_resp = client.new_listen_key()
 ws_client.partial_book_depth(
     symbol=SYMBOL,
     id=1,
@@ -53,13 +53,8 @@ ws_client.partial_book_depth(
     speed=100,
     callback=book_update_handler,
 )
-
-response = client.new_listen_key()
-
-# res = client.get_open_orders(symbol="BTCUSDT", orderId=35298599362)
-# print([r for r in res if "BTC" in r['symbol']])
 ws_client.user_data(
-    listen_key=response["listenKey"],
+    listen_key=listen_key_resp["listenKey"],
     id=1,
     callback=account_update_handler,
 )
--- portfolio.py
+++ portfolio.py
@@ -1,46 +1,59 @@
 import json
 import os
+import time
+import threading
 from apis import SYMBOL, client
 from infra.rich_logging import logging, getLogger, print
 
-
-class Portfolio:
+class Portfolio():
     def __init__(self, config: dict):
-        self.should_ask = True
-        self.should_bid = True
         self.quotes = {"ask_px": 0, "bid_px": 0}
-        self.config = config["config"]
-        self._name = config["name"]
+        self.config = config['config']
+        self._name = config['name']
         self._logger = getLogger(f"Portfolio {self._name}")
         self._logger.setLevel("INFO")
-        current_exposure = [
-            position
-            for position in client.get_position_risk()
-            if position["symbol"] == SYMBOL
-        ][0]
-        self.position_size = float(current_exposure["positionAmt"])
+
+        # used for inventory management
+        self.position_size = 0 
+        self.should_ask = True
+        self.should_bid = True
+
+        # used to keep tract of orders
         self.asks = []
         self.bids = []
 
-        self._logger.info(
-            f"Initiated portfolio {self._name} with parameters {self.config}"
-        )
+        self._logger.info(f"Initiated portfolio {self._name} with parameters {self.config}")
         self.reset()
 
-    def reset(self):
-        open_orders = [
-            order
-            for order in client.get_all_orders(symbol=SYMBOL)
-            if order["status"] not in ["FILLED", "CANCELED"]
-        ]
-        self.asks = [order for order in open_orders if order["side"] == "SELL"]
-        self.bids = [order for order in open_orders if order["side"] == "BUY"]
+        current_exposure = [position for position in client.get_position_risk() if position['symbol'] == SYMBOL][0]
+        self.position_size = float(current_exposure['positionAmt'])
 
-        self._logger.debug(f"Reset complete. \nasks: {self.asks}\nbids: {self.bids}")
+        self.worker_threads = [
+            threading.Thread(target=self.reset_thread)
+        ]
+        for i in self.worker_threads:
+            i.start()
+
+
+    def reset_thread(self):
+        while True:
+            self.reset()
+            time.sleep(1)
+    
+    def reset(self) -> None:
+        """
+        Function pulls APIs to reflect the most up-to-date open asks and bids as well as exposure.
+        """
+        open_orders = [order for order in client.get_all_orders(symbol=SYMBOL) if order['status'] not in ['FILLED', 'CANCELED']]
+        self.asks = [order for order in open_orders if order['side'] == "SELL"]
+        self.bids = [order for order in open_orders if order['side'] == "BUY"]
+
+
+        self._logger.debug(f"Reset complete.")
 
     def update_quotes(self, new_quotes):
         if self.quotes != new_quotes:
-            self._logger.info("updating quotes")
+            self._logger.debug("updating quotes")
             asks_to_submit = []
             bids_to_submit = []
             asks_to_cancel = []
@@ -50,71 +63,60 @@ class Portfolio:
             # in this case, we don't need to worry about being the top quoter
             # because price level already changed.
             ask_px_changed = bid_px_changed = False
-            if new_quotes["ask_px"] != self.quotes["ask_px"]:
+            if new_quotes['ask_px'] != self.quotes['ask_px']:
                 ask_px_changed = True
-                self._logger.debug("Should adjust asks")
-                asks_to_cancel.extend([ask["orderId"] for ask in self.asks])
+                self._logger.info("Ask price changed: adjusting quotes")
+                asks_to_cancel.extend([ask['orderId'] for ask in self.asks])
                 self.asks = []
                 asks_to_submit.extend(self._create_new_asks_params(new_quotes))
-            if new_quotes["bid_px"] != self.quotes["bid_px"]:
+            if new_quotes['bid_px'] != self.quotes['bid_px']:
                 bid_px_changed = True
-                self._logger.debug("Should adjust bids")
-                bids_to_cancel.extend([bid["orderId"] for bid in self.bids])
+                self._logger.info("Bid price changed: adjusting quotes")
+                bids_to_cancel.extend([bid['orderId'] for bid in self.bids])
                 self.bids = []
                 bids_to_submit.extend(self._create_new_bids_params(new_quotes))
             self.quotes = new_quotes
-
+            
             # pull and retreat to secondary price level if we're the only top book quoter
             if not ask_px_changed:
                 for ask in self.asks:
-                    if float(ask["price"]) == float(new_quotes["ask_px"]) and float(
-                        ask["origQty"]
-                    ) > float(new_quotes["ask_qty"]):
-                        self._logger.warning(
-                            "We're the only one quoting on the ask side! Retreating to secondary level"
-                        )
-                        asks_to_cancel.append(ask["orderId"])
+                    if float(ask['price']) == float(new_quotes['ask_px']) and float(ask['origQty']) >= float(new_quotes['ask_qty']):
+                        self._logger.warning(f"We're the only one quoting on the ask side! Retreating to secondary level {new_quotes['ask_px']} -> {new_quotes['secondary_ask_px']}")
+                        asks_to_cancel.append(ask['orderId'])
                         try:
-                            self.asks.remove(ask["orderId"])
+                            self.asks.remove(ask['orderId'])
                         except:
                             pass
                         try:
-                            asks_to_submit.append(
-                                {
-                                    "symbol": SYMBOL.lower(),
-                                    "side": "SELL",
-                                    "type": "LIMIT",
-                                    "quantity": str(ask["origQty"]),
-                                    "timeInForce": "GTC",
-                                    "price": str(new_quotes["secondary_ask_px"]),
-                                }
-                            )
+                            asks_to_submit.append({
+                                "symbol": SYMBOL.lower(),
+                                "side": "SELL",
+                                "type": "LIMIT",
+                                "quantity": str(ask['origQty']),
+                                "timeInForce": "GTC",
+                                "price": str(new_quotes['secondary_ask_px'])
+                            })
                         except Exception as e:
                             pass
+
             if not bid_px_changed:
                 for bid in self.bids:
-                    if float(bid["price"]) == float(new_quotes["bid_px"]) and float(
-                        bid["origQty"]
-                    ) > float(new_quotes["bid_qty"]):
-                        self._logger.warning(
-                            "We're the only one quoting on the bid side! Retreating to secondary level"
-                        )
-                        bids_to_cancel.append(bid["orderId"])
+                    if float(bid['price']) == float(new_quotes['bid_px']) and float(bid['origQty']) >= float(new_quotes['bid_qty']):
+                        self._logger.warning(f"We're the only one quoting on the bid side! Retreating to secondary level {new_quotes['bid_px']} -> {new_quotes['secondary_bid_px']}")
+                        bids_to_cancel.append(bid['orderId'])
                         try:
-                            self.bids.remove(bid["orderId"])
+                            self.bids.remove(bid['orderId'])
                         except:
                             pass
                         try:
-                            bids_to_submit.append(
-                                {
-                                    "symbol": SYMBOL.lower(),
-                                    "side": "BUY",
-                                    "type": "LIMIT",
-                                    "quantity": str(bid["origQty"]),
-                                    "timeInForce": "GTC",
-                                    "price": str(new_quotes["secondary_bid_px"]),
-                                }
-                            )
+                            bids_to_submit.append({
+                                "symbol": SYMBOL.lower(),
+                                "side": "BUY",
+                                "type": "LIMIT",
+                                "quantity": str(bid['origQty']),
+                                "timeInForce": "GTC",
+                                "price": str(new_quotes['secondary_bid_px'])
+                            })
                         except Exception as e:
                             pass
 
@@ -122,10 +124,7 @@ class Portfolio:
             orders_to_cancel = bids_to_cancel + asks_to_cancel
             if len(orders_to_cancel) > 0:
                 response = client.cancel_batch_order(
-                    symbol="BTCUSDT",
-                    orderIdList=orders_to_cancel,
-                    origClientOrderIdList=orders_to_cancel,
-                    recvWindow=2000,
+                    symbol="BTCUSDT", orderIdList=orders_to_cancel, origClientOrderIdList=orders_to_cancel, recvWindow=2000
                 )
                 self._logger.debug("cancelled orders")
 
@@ -133,7 +132,7 @@ class Portfolio:
             if len(orders_to_submit) > 0:
                 self._logger.debug("submitting orders")
                 new_orders = client.new_batch_order(batchOrders=orders_to_submit)
-                self.reset()
+            self.reset()
 
     def update_position_size(self, position_size):
         self.position_size = position_size
@@ -141,38 +140,34 @@ class Portfolio:
 
     def _create_new_asks_params(self, new_quotes):
         params = []
-        self.should_ask = self.position_size > self.config["inventory"]["short_limit"]
+        self.should_ask = self.position_size > self.config['inventory']['short_limit']
         if self.should_ask:
-            for ask in self.config["quotes"]["asks"]:
-                params.append(
-                    {
-                        "symbol": SYMBOL.lower(),
-                        "side": "SELL",
-                        "type": "LIMIT",
-                        "quantity": str(ask["size"]),
-                        "timeInForce": "GTC",
-                        "price": str(new_quotes["ask_px"] + ask["offset"]),
-                    }
-                )
+            for ask in self.config['quotes']['asks']:
+                params.append({
+                    "symbol": SYMBOL.lower(),
+                    "side": "SELL",
+                    "type": "LIMIT",
+                    "quantity": str(ask['size']),
+                    "timeInForce": "GTC",
+                    "price": str(new_quotes['ask_px'] + ask['offset'])
+                })
         else:
-            self._logger.warning("Not asking")
+            self._logger.warning(f"Not asking: Current exposure {self.position_size}")
         return params
 
     def _create_new_bids_params(self, new_quotes):
         params = []
-        self.should_bid = self.position_size < self.config["inventory"]["long_limit"]
+        self.should_bid = self.position_size < self.config['inventory']['long_limit']
         if self.should_bid:
-            for bid in self.config["quotes"]["bids"]:
-                params.append(
-                    {
-                        "symbol": SYMBOL.lower(),
-                        "side": "BUY",
-                        "type": "LIMIT",
-                        "quantity": str(bid["size"]),
-                        "timeInForce": "GTC",
-                        "price": str(new_quotes["bid_px"] + bid["offset"]),
-                    }
-                )
+            for bid in self.config['quotes']['bids']:
+                params.append({
+                    "symbol": SYMBOL.lower(),
+                    "side": "BUY",
+                    "type": "LIMIT",
+                    "quantity": str(bid['size']),
+                    "timeInForce": "GTC",
+                    "price": str(new_quotes['bid_px'] + bid['offset'])
+                })
         else:
-            self._logger.warning("Not bidding")
+            self._logger.warning(f"Not bidding: Current exposure: {self.position_size}")
         return params
